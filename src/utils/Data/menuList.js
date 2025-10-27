e210acb000425627eea457e894e88b816699d8cc
Maulik Patel <mapatel90@gmail.com>
2025-10-27 11:50:03 +0530

    users menu add

--- src/utils/Data/menuList.js
+++ src/utils/Data/menuList.js
@@ -20,257 +20,97 @@ export const menuList = [
         ]
     },
     // {
-    //     id: 1,
-    //     name: "reports",
+    //     id: 4,
+    //     name: "payment",
     //     path: "#",
-    //     icon: 'feather-cast',
+    //     icon: 'feather-dollar-sign',
     //     dropdownMenu: [
     //         {
     //             id: 1,
-    //             name: "Sales Report",
-    //             path: "/reports/sales",
+    //             name: "payment",
+    //             path: "/admin/payment/list",
     //             subdropdownMenu: false
     //         },
     //         {
     //             id: 2,
-    //             name: "Leads Report",
-    //             path: "/reports/leads",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 3,
-    //             name: "Project Report",
-    //             path: "/reports/project",
+    //             name: "invoiceview",
+    //             path: "/admin/payment/view",
     //             subdropdownMenu: false
     //         },
     //         {
     //             id: 4,
-    //             name: "Timesheets Report",
-    //             path: "/reports/timesheets",
+    //             name: "invoicecreate",
+    //             path: "/admin/payment/create",
     //             subdropdownMenu: false
-    //         },
-
+    //         }
     //     ]
     // },
     // {
-    //     id: 2,
-    //     name: "applications",
-    //     path: '#',
-    //     icon: 'feather-send',
+    //     id: 5,
+    //     name: "customers",
+    //     path: "#",
+    //     icon: 'feather-users',
     //     dropdownMenu: [
     //         {
     //             id: 1,
-    //             name: "Chat",
-    //             path: "/applications/chat",
+    //             name: "customers",
+    //             path: "/admin/customers/list",
     //             subdropdownMenu: false
     //         },
     //         {
     //             id: 2,
-    //             name: "Email",
-    //             path: "/applications/email",
+    //             name: "customersview",
+    //             path: "/admin/customers/view",
     //             subdropdownMenu: false
     //         },
     //         {
     //             id: 3,
-    //             name: "Tasks",
-    //             path: "/applications/tasks",
+    //             name: "customerscreate",
+    //             path: "/admin/customers/create",
     //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 4,
-    //             name: "Notes",
-    //             path: "/applications/notes",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 5,
-    //             name: "Storage",
-    //             path: "/applications/storage",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 6,
-    //             name: "Calendar",
-    //             path: "/applications/calendar",
-    //             subdropdownMenu: false
-    //         },
+    //         }
     //     ]
     // },
     // {
-    //     id: 3,
-    //     name: "proposal",
+    //     id: 7,
+    //     name: "projects",
     //     path: "#",
-    //     icon: 'feather-at-sign',
+    //     icon: 'feather-briefcase',
     //     dropdownMenu: [
     //         {
     //             id: 1,
-    //             name: "Proposal",
-    //             path: "/proposal/list",
+    //             name: "projects",
+    //             path: "/admin/projects/list",
     //             subdropdownMenu: false
     //         },
     //         {
     //             id: 2,
-    //             name: "Proposal View",
-    //             path: "/proposal/view",
+    //             name: "projectsview",
+    //             path: "/admin/projects/view",
     //             subdropdownMenu: false
     //         },
     //         {
     //             id: 3,
-    //             name: "Proposal Edit",
-    //             path: "/proposal/edit",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 4,
-    //             name: "Proposal Create",
-    //             path: "/proposal/create",
-    //             subdropdownMenu: false
-    //         },
-
-    //     ],
-    // },
-    {
-        id: 4,
-        name: "payment",
-        path: "#",
-        icon: 'feather-dollar-sign',
-        dropdownMenu: [
-            {
-                id: 1,
-                name: "payment",
-                path: "/admin/payment/list",
-                subdropdownMenu: false
-            },
-            {
-                id: 2,
-                name: "invoiceview",
-                path: "/admin/payment/view",
-                subdropdownMenu: false
-            },
-            {
-                id: 4,
-                name: "invoicecreate",
-                path: "/admin/payment/create",
-                subdropdownMenu: false
-            }
-        ]
-    },
-    {
-        id: 5,
-        name: "customers",
-        path: "#",
-        icon: 'feather-users',
-        dropdownMenu: [
-            {
-                id: 1,
-                name: "customers",
-                path: "/admin/customers/list",
-                subdropdownMenu: false
-            },
-            {
-                id: 2,
-                name: "customersview",
-                path: "/admin/customers/view",
-                subdropdownMenu: false
-            },
-            {
-                id: 3,
-                name: "customerscreate",
-                path: "/admin/customers/create",
-                subdropdownMenu: false
-            }
-        ]
-    },
-    // {
-    //     id: 6,
-    //     name: "leads",
-    //     path: "#",
-    //     icon: 'feather-alert-circle',
-    //     dropdownMenu: [
-    //         {
-    //             id: 1,
-    //             name: "Leads",
-    //             path: "/leads/list",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 2,
-    //             name: "Leads View",
-    //             path: "/leads/view",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 3,
-    //             name: "Leads Create",
-    //             path: "/leads/create",
+    //             name: "projectscreate",
+    //             path: "/admin/projects/create",
     //             subdropdownMenu: false
     //         }
     //     ]
     // },
     {
-        id: 7,
-        name: "projects",
+        id: 8,
+        name: "UserManagement",
         path: "#",
-        icon: 'feather-briefcase',
+        icon: 'feather-users',
         dropdownMenu: [
             {
                 id: 1,
-                name: "projects",
-                path: "/admin/projects/list",
+                name: "users",
+                path: "/admin/payment/list",
                 subdropdownMenu: false
             },
-            {
-                id: 2,
-                name: "projectsview",
-                path: "/admin/projects/view",
-                subdropdownMenu: false
-            },
-            {
-                id: 3,
-                name: "projectscreate",
-                path: "/admin/projects/create",
-                subdropdownMenu: false
-            }
         ]
     },
-    // {
-    //     id: 8,
-    //     name: "widgets",
-    //     path: "#",
-    //     icon: 'feather-layout',
-    //     dropdownMenu: [
-    //         {
-    //             id: 1,
-    //             name: "Lists",
-    //             path: "/widgets/lists",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 2,
-    //             name: "Tables",
-    //             path: "/widgets/tables",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 3,
-    //             name: "Charts",
-    //             path: "/widgets/charts",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 4,
-    //             name: "Statistics",
-    //             path: "/widgets/statistics",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 5,
-    //             name: "Miscellaneous",
-    //             path: "/widgets/miscellaneous",
-    //             subdropdownMenu: false
-    //         },
-    //     ]
-    // },
     {
         id: 9,
         name: "settings",
@@ -363,172 +203,5 @@ export const menuList = [
             }
         ]
     },
-    // {
-    //     id: 10,
-    //     name: "authentication",
-    //     path: "#",
-    //     icon: 'feather-power',
-    //     dropdownMenu: [
-    //         {
-    //             id: 1,
-    //             name: "login",
-    //             path: "#",
-    //             subdropdownMenu: [
-    //                 {
-    //                     id: 1,
-    //                     name: "Cover",
-    //                     path: "/authentication/login/cover",
-    //                 },
-    //                 {
-    //                     id: 2,
-    //                     name: "Minimal",
-    //                     path: "/authentication/login/minimal",
-    //                 },
-    //                 {
-    //                     id: 3,
-    //                     name: "Creative",
-    //                     path: "/authentication/login/creative",
-    //                 },
-    //             ]
-    //         },
-    //         {
-    //             id: 2,
-    //             name: "register",
-    //             path: "#",
-    //             subdropdownMenu: [
-    //                 {
-    //                     id: 1,
-    //                     name: "Cover",
-    //                     path: "/authentication/register/cover",
-    //                 },
-    //                 {
-    //                     id: 2,
-    //                     name: "Minimal",
-    //                     path: "/authentication/register/minimal",
-    //                 },
-    //                 {
-    //                     id: 3,
-    //                     name: "Creative",
-    //                     path: "/authentication/register/creative",
-    //                 },
-    //             ]
-    //         },
-    //         {
-    //             id: 3,
-    //             name: "Error 404",
-    //             path: "#",
-    //             subdropdownMenu: [
-    //                 {
-    //                     id: 1,
-    //                     name: "Cover",
-    //                     path: "/authentication/404/cover",
-    //                 },
-    //                 {
-    //                     id: 2,
-    //                     name: "Minimal",
-    //                     path: "/authentication/404/minimal",
-    //                 },
-    //                 {
-    //                     id: 3,
-    //                     name: "Creative",
-    //                     path: "/authentication/404/creative",
-    //                 },
-    //             ]
-    //         },
-    //         {
-    //             id: 4,
-    //             name: "Reset Pass",
-    //             path: "#",
-    //             subdropdownMenu: [
-    //                 {
-    //                     id: 1,
-    //                     name: "Cover",
-    //                     path: "/authentication/reset/cover",
-    //                 },
-    //                 {
-    //                     id: 2,
-    //                     name: "Minimal",
-    //                     path: "/authentication/reset/minimal",
-    //                 },
-    //                 {
-    //                     id: 3,
-    //                     name: "Creative",
-    //                     path: "/authentication/reset/creative",
-    //                 },
-    //             ]
-    //         },
-    //         {
-    //             id: 5,
-    //             name: "Verify OTP",
-    //             path: "#",
-    //             subdropdownMenu: [
-    //                 {
-    //                     id: 1,
-    //                     name: "Cover",
-    //                     path: "/authentication/verify/cover",
-    //                 },
-    //                 {
-    //                     id: 2,
-    //                     name: "Minimal",
-    //                     path: "/authentication/verify/minimal",
-    //                 },
-    //                 {
-    //                     id: 3,
-    //                     name: "Creative",
-    //                     path: "/authentication/verify/creative",
-    //                 },
-    //             ]
-    //         },
-    //         {
-    //             id: 6,
-    //             name: "Maintenance",
-    //             path: "#",
-    //             subdropdownMenu: [
-    //                 {
-    //                     id: 1,
-    //                     name: "Cover",
-    //                     path: "/authentication/maintenance/cover",
-    //                 },
-    //                 {
-    //                     id: 2,
-    //                     name: "Minimal",
-    //                     path: "/authentication/maintenance/minimal",
-    //                 },
-    //                 {
-    //                     id: 3,
-    //                     name: "Creative",
-    //                     path: "/authentication/maintenance/creative",
-    //                 },
-    //             ]
-    //         },
-    //     ]
-    // },
-    // {
-    //     id: 11,
-    //     name: "help center",
-    //     path: "#",
-    //     icon: 'feather-life-buoy',
-    //     dropdownMenu: [
-    //         {
-    //             id: 1,
-    //             name: "Support",
-    //             path: "https://wrapbootstrap.com/user/theme_ocean",
-    //             target:"_blank",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 2,
-    //             name: "KnowledgeBase",
-    //             path: "/help/knowledgebase",
-    //             subdropdownMenu: false
-    //         },
-    //         {
-    //             id: 3,
-    //             name: "Documentations",
-    //             path: "https://duralux-next-doc.vercel.app/",
-    //             target:"_blank",
-    //             subdropdownMenu: false
-    //         }
-    //     ]
-    // },
+   
 ]
